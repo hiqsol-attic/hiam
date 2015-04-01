dfa96bc7ca989835f135459fda56d68813e8cfe0
Andrey Klochok <andreyklochok@gmail.com>
2015-04-01 18:57:09 +0300

    Do real form in login

--- frontend/themes/adminlte/site/login.php
+++ frontend/themes/adminlte/site/login.php
@@ -6,6 +6,7 @@
  * Time: 17:29
  */
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 
 $this->blocks['bodyClass'] = 'login-page';
 $this->title = 'Sign in';
@@ -13,28 +14,26 @@ $this->title = 'Sign in';
 
 <div class="login-box-body">
     <p class="login-box-msg">Sign in to start your session</p>
-    <form action="../../index2.html" method="post">
+    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
         <div class="form-group has-feedback">
-            <input type="text" class="form-control" placeholder="Email"/>
+            <?= $form->field($model, 'username')->textInput(['placeholder' => 'Login or Email', 'class' => 'form-control'])->label(false); ?>
             <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
         </div>
         <div class="form-group has-feedback">
-            <input type="password" class="form-control" placeholder="Password"/>
+            <?= $form->field($model, 'password')->textInput(['placeholder' => 'Password', 'class' => 'form-control'])->label(false); ?>
             <span class="glyphicon glyphicon-lock form-control-feedback"></span>
         </div>
         <div class="row">
             <div class="col-xs-8">
-                <div class="checkbox icheck">
-                    <label>
-                        <input type="checkbox"> Remember Me
-                    </label>
+                <div class="icheck">
+                        <?= $form->field($model, 'rememberMe')->checkbox([])->label(false); ?> Remember Me
                 </div>
             </div><!-- /.col -->
             <div class="col-xs-4">
                 <button type="submit" class="btn btn-primary btn-block btn-flat">Sign In</button>
             </div><!-- /.col -->
         </div>
-    </form>
+    <?php $form::end(); ?>
 
     <div class="social-auth-links text-center">
         <p>- OR -</p>
