7c54f96b1df3566afccff25eb823663afc635b56
sol <sol@hiqdev.com>
2015-04-03 10:19:16 +0000

    * lockscreen

--- frontend/themes/adminlte/site/lockscreen.php
+++ frontend/themes/adminlte/site/lockscreen.php
@@ -12,27 +12,27 @@ $this->title = 'Lockscreen';
 ?>
 
 <!-- User name -->
-<div class="lockscreen-name">John Doe</div>
+<div class="lockscreen-name"><?= \Yii::$app->user->identity->login ?></div>
 
 <!-- START LOCK SCREEN ITEM -->
 <div class="lockscreen-item">
     <!-- lockscreen image -->
     <div class="lockscreen-image">
-        <img src="https://almsaeedstudio.com/themes/AdminLTE/dist/img/user1-128x128.jpg" alt="user image"/>
+        <?= $this->render('/layouts/gravatar',['size'=>128]) ?>
     </div>
     <!-- /.lockscreen-image -->
 
     <!-- lockscreen credentials (contains the form) -->
     <form class="lockscreen-credentials">
-        <input type="submit" class="form-control btn" value="Yes" />
+        <input type="submit" class="form-control btn" value="Return to HiPanel" />
     </form><!-- /.lockscreen credentials -->
 
 </div><!-- /.lockscreen-item -->
-<div class="help-block text-center">
+<!-- div class="help-block text-center">
     Enter your password to retrieve your session
-</div>
+</div -->
 <div class='text-center'>
-    <?= Html::a('Or logout and sign in as a different user', ['/site/logout']); ?>
+    <b><?= Html::a('Or logout and sign in as a different user', ['/site/logout']); ?></b>
 </div>
 <div class='lockscreen-footer text-center'>
     Copyright &copy; 2014-2015 <b><a href="http://almsaeedstudio.com" class='text-black'><?= Yii::$app->name; ?></a></b><br>
