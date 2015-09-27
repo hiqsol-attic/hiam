ca894636439ebab6efad7591beae1a2101133b20
Andrii Vasyliev <sol@hiqdev.com>
2015-09-27 16:01:20 +0000

    + basic about page

--- src/frontend/views/site/about.php
+++ src/frontend/views/site/about.php
@@ -1,14 +1,23 @@
 <?php
+
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = 'About';
+$this->title = Yii::t('app', 'Powered by HIAM');
 $this->params['breadcrumbs'][] = $this->title;
+
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>This is the About page. You may modify the following file to customize its content:</p>
-
-    <code><?= __FILE__ ?></code>
+    <p>HIAM is Identity and Access Management server providing OAuth2, ABAC and logging</p>
+    <ul>
+        <li>IAM - Identity and Access Management</li>
+        <li>ABAC - Attribute Based Access Control</li>
+        <li>Social login: Facebook, Google, VK, LinkedIn, GitHub, Live, Yandex</li>
+        <li>Full activity logging with searching and reporting</li>
+    </ul>
+    <p>Open source. BSD-3-Clause license.</p>
+    <p>Copyright © 2014-2015, HiQDev (<a href="https://hiqdev.com/">https://hiqdev.com/</a>)</p>
+    <p>Available at <a href="https://github.com/hiqdev/hiam-core">GitHub</a>.</p>
 </div>
