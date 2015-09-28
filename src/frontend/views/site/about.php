d9a0284da7b3322273a4152eaeab1b3d6164ea2a
Andrii Vasyliev <sol@hiqdev.com>
2015-09-28 07:28:08 +0000

    redone about: made more shy

--- src/frontend/views/site/about.php
+++ src/frontend/views/site/about.php
@@ -3,21 +3,13 @@
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
-$this->title = Yii::t('app', 'Powered by HIAM');
+$this->title = Yii::t('app', 'About');
 $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>HIAM is Identity and Access Management server providing OAuth2, ABAC and logging</p>
-    <ul>
-        <li>IAM - Identity and Access Management</li>
-        <li>ABAC - Attribute Based Access Control</li>
-        <li>Social login: Facebook, Google, VK, LinkedIn, GitHub, Live, Yandex</li>
-        <li>Full activity logging with searching and reporting</li>
-    </ul>
-    <p>Open source. BSD-3-Clause license.</p>
-    <p>Copyright © 2014-2015, HiQDev (<a href="https://hiqdev.com/">https://hiqdev.com/</a>)</p>
-    <p>Available at <a href="https://github.com/hiqdev/hiam-core">GitHub</a>.</p>
+    <p>This is Identity and Access Management server of <?= Yii::$app->params['orgName'] ?></p>
+    <p>See more at <a href="<?= Yii::$app->params['orgUrl'] ?>"><?= Yii::$app->params['orgUrl'] ?></a></p>
 </div>
