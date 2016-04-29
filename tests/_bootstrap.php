d1355119e2adb85c57c8719b4251b7eee9665be9
Andrii Vasyliev <sol@hiqdev.com>
2016-04-29 13:48:49 +0000

    inited tests

---
+++ tests/_bootstrap.php
@@ -0,0 +1,7 @@
+<?php
+
+error_reporting(E_ALL & ~E_NOTICE);
+
+$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+
+require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
