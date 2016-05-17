29de7f8c33f29b0aa247ab95ace8a6ca6ee876c0
Andrii Vasyliev <sol@hiqdev.com>
2016-05-17 11:50:24 +0000

    minor

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -1,7 +1,16 @@
 <?php
 
+/*
+ * Identity and Access Management server providing OAuth2, RBAC and logging
+ *
+ * @link      https://github.com/hiqdev/hiam-core
+ * @package   hiam-core
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2014-2016, HiQDev (http://hiqdev.com/)
+ */
+
 error_reporting(E_ALL & ~E_NOTICE);
 
-$bootstrap = __DIR__ . '/../src/_bootstrap.php';
+$bootstrap = __DIR__ . '/../src/config/bootstrap.php';
 
 require_once file_exists($bootstrap) ? $bootstrap : __DIR__ . '/../vendor/autoload.php';
