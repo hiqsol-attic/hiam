dd92e11fe3272a428e6bca5b69f440119823176a
Andrey Klochok <andreyklochok@gmail.com>
2018-07-25 13:14:42 +0300

    Added `i_agree_pp` boolean attribute to SignupForm, cause there was a need to agree with the privacy policy during registration.

--- src/forms/SignupForm.php
+++ src/forms/SignupForm.php
@@ -24,6 +24,7 @@ class SignupForm extends \yii\base\Model
     public $password;
     public $password_retype;
     public $i_agree;
+    public $i_agree_pp;
 
     /**
      * {@inheritdoc}
@@ -47,8 +48,9 @@ class SignupForm extends \yii\base\Model
 
             [['first_name', 'last_name', 'email', 'password', 'password_retype'], 'required'],
 
-            ['i_agree', 'boolean'],
+            [['i_agree', 'i_agree_pp'], 'boolean'],
             ['i_agree', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider terms of use.')],
+            ['i_agree_pp', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider Privacy Policy.')],
         ];
     }
 
@@ -61,6 +63,7 @@ class SignupForm extends \yii\base\Model
             'password' => Yii::t('hiam', 'Password'),
             'password_retype' => Yii::t('hiam', 'Retype password'),
             'i_agree' => Yii::t('hiam', 'I agree to the') . ' ' . Html::a(Yii::t('hiam', 'terms'), '/site/terms'),
+            'i_agree_pp' => Yii::t('hiam', 'I agree to the') . ' ' . Html::a(Yii::t('hiam', 'Privacy Policy'), Yii::$app->params['legals.privacyPolicyUrl']),
         ];
     }
 }
