280f0e3a77e424813548fac05e734270bc51bc09
Andrey Klochok <andreyklochok@gmail.com>
2018-07-25 13:44:25 +0300

    refactored

--- src/forms/SignupForm.php
+++ src/forms/SignupForm.php
@@ -24,7 +24,7 @@ class SignupForm extends \yii\base\Model
     public $password;
     public $password_retype;
     public $i_agree;
-    public $i_agree_pp;
+    public $i_agree_privacy_policy;
 
     /**
      * {@inheritdoc}
@@ -48,9 +48,9 @@ class SignupForm extends \yii\base\Model
 
             [['first_name', 'last_name', 'email', 'password', 'password_retype'], 'required'],
 
-            [['i_agree', 'i_agree_pp'], 'boolean'],
+            [['i_agree', 'i_agree_privacy_policy'], 'boolean'],
             ['i_agree', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider terms of use.')],
-            ['i_agree_pp', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider Privacy Policy.')],
+            ['i_agree_privacy_policy', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider Privacy Policy.')],
         ];
     }
 
@@ -63,7 +63,7 @@ class SignupForm extends \yii\base\Model
             'password' => Yii::t('hiam', 'Password'),
             'password_retype' => Yii::t('hiam', 'Retype password'),
             'i_agree' => Yii::t('hiam', 'I agree to the') . ' ' . Html::a(Yii::t('hiam', 'terms'), '/site/terms'),
-            'i_agree_pp' => Yii::t('hiam', 'I agree to the') . ' ' . Html::a(Yii::t('hiam', 'Privacy Policy'), Yii::$app->params['legals.privacyPolicyUrl']),
+            'i_agree_privacy_policy' => Yii::t('hiam', 'I agree to the') . ' ' . Html::a(Yii::t('hiam', 'Privacy Policy'), '/site/privacy-policy'),
         ];
     }
 }
