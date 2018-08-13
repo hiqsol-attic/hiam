1d80fbca42ac4bcab4f8a7af8182801464b54707
Andrey Klochok <andreyklochok@gmail.com>
2018-08-13 13:41:27 +0300

    translations

--- src/messages/ru/hiam.php
+++ src/messages/ru/hiam.php
@@ -56,4 +56,8 @@ return [
     'Incorrect username or password.' => 'Неверный логин или пароль.',
     'Privacy Policy' => 'Политикой Конфиденциальности',
     'Please consider Privacy Policy.' => 'Пожалуйста, учтите Политику Конфиденциальности.',
+    'Email has been confirmed!' => 'Электронная почта подтверждена!',
+    'Error. Email has not been confirmed!' => 'Ошибка. Email не был подтвержден!',
+    'Thank you, now you can {link}.' => 'Спасибо, теперь Вы можете {link}.',
+    'return to panel' => 'вернуться в панель',
 ];
