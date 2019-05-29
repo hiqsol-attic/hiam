1106e5c0e4c0d0f377fd4a1f7d355cac805b6bb3
Andrey Klochok <andreyklochok@gmail.com>
2019-05-29 18:24:21 +0300

    Refactored SiteController and SignupForm for new `advancedhosting` theme

--- src/forms/SignupForm.php
+++ src/forms/SignupForm.php
@@ -11,20 +11,31 @@
 namespace hiam\forms;
 
 use Yii;
+use yii\base\Model;
 use yii\helpers\Html;
 
 /**
  * Signup form.
  */
-class SignupForm extends \yii\base\Model
+class SignupForm extends Model
 {
     public $first_name;
+
     public $last_name;
+
     public $email;
+
     public $password;
+
     public $password_retype;
+
     public $i_agree;
+
     public $i_agree_privacy_policy;
+
+    public $i_agree_terms_and_privacy;
+
+    public $send_me_news;
 
     /**
      * {@inheritdoc}
@@ -46,11 +57,13 @@ class SignupForm extends \yii\base\Model
 
             ['password_retype', 'compare', 'compareAttribute' => 'password', 'message' => Yii::t('hiam', "Passwords don't match.")],
 
-            [['first_name', 'last_name', 'email', 'password', 'password_retype'], 'required'],
+            [['email', 'password'], 'required', 'on' => ['default', 'short']],
+            [['first_name', 'last_name', 'password_retype'], 'required', 'on' => ['default']],
 
-            [['i_agree', 'i_agree_privacy_policy'], 'boolean'],
-            ['i_agree', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider terms of use.')],
-            ['i_agree_privacy_policy', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider Privacy Policy.')],
+            [['i_agree', 'i_agree_privacy_policy', 'i_agree_terms_and_privacy', 'send_me_news'], 'boolean'],
+            ['i_agree', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider terms of use.'), 'on' => ['default']],
+            ['i_agree_privacy_policy', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider Privacy Policy.'), 'on' => ['default']],
+            ['i_agree_terms_and_privacy', 'required', 'requiredValue' => 1, 'message' => Yii::t('hiam', 'Please consider Terms of Use, Privacy Policy and cookies.'), 'on' => ['short', 'social']],
         ];
     }
 
@@ -64,6 +77,11 @@ class SignupForm extends \yii\base\Model
             'password_retype' => Yii::t('hiam', 'Retype password'),
             'i_agree' => Yii::t('hiam', 'I agree to the') . ' ' . Html::a(Yii::t('hiam', 'terms'), '/site/terms'),
             'i_agree_privacy_policy' => Yii::t('hiam', 'I agree to the') . ' ' . Html::a(Yii::t('hiam', 'Privacy Policy'), '/site/privacy-policy'),
+            'send_me_news' => Yii::t('hiam', 'Send me important news and special offers'),
+            'i_agree_terms_and_privacy' => Yii::t('hiam', 'I agree to the {terms_of_services}, {privacy_policy} and the collection of cookies on the website', [
+                'terms_of_services' => Html::a(Yii::t('hiam', 'Terms of Services'), '/site/terms'),
+                'privacy_policy' => Html::a(Yii::t('hiam', 'Privacy Policy'), '/site/privacy-policy'),
+            ]),
         ];
     }
 }
